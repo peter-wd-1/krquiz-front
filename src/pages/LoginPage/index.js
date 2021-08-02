390fe36d52a71f025e765e6e6d39ddfe8167c1fd
Peter <peterismic@gmail.com>
2021-08-02 11:58:36 -0400

    add home button on login page

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
 import { LoginForm } from "components/LoginForm";
+import { LogoutButton } from "pages/ProfilePage/lib";
 import xicon from "image/xicon.png";
 function LoginPage() {
     return (
         <div>
             <LoginForm />
+            <LogoutButton>HOME</LogoutButton>
         </div>
     );
 }
